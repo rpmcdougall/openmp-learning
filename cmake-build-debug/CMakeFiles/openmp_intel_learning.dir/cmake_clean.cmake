0ad67cff7a97d253a9327df1f57f4d4467146909
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-10 17:44:59 -0500

    init + hello world

---
+++ cmake-build-debug/CMakeFiles/openmp_intel_learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openmp_intel_learning.dir/main.c.o"
+  "openmp_intel_learning.pdb"
+  "openmp_intel_learning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/openmp_intel_learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
