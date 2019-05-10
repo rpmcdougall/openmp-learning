828319af2aad4bbd98a549e0029ad30527706052
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-10 18:51:26 -0500

    SPMD with syncronizaton using critical

--- main.c
+++ main.c
@@ -15,36 +15,35 @@
 //    return 0;
 //}
 
-
 /*
- * Threading (Fork Join), Pi SPMD Strategy
+ * Threading (Fork Join), Pi SPMD Strategy, with Synchronization
  */
 static long num_steps = 100000;
 double step;
 #define NUM_THREADS 2
 void main() {
-    int i, nthreads; double pi, sum[NUM_THREADS];
+    double pi = 0.0;
     step = 1.0/(double) num_steps;
 
     omp_set_num_threads(NUM_THREADS);
     double start_time = omp_get_wtime();
     #pragma omp parallel
     {
-        int i, id, nthrds;
-        double x;
+        int i, id, nthrds, nthreads;
+        double x, sum;
         id = omp_get_thread_num();
         nthrds = omp_get_num_threads();
         if (id == 0) nthreads = nthrds;
-        for (i=0, sum[id] = 0.0; i<num_steps; i=i+nthrds) {
+        for (i=0, sum = 0.0; i<num_steps; i=i+nthreads) {
             x = (i + 0.5) * step;
-            sum[id] += 4.0/(1.0 + x*x);
+            sum += 4.0/(1.0 + x*x);
         }
+        // Only one thread summing at a time into pi
+        #pragma omp critical
+            pi += sum * step;
     };
 
     double end_time = omp_get_wtime();
-    for(i=0, pi =0.0; i < nthreads; i++) {
-        pi += sum[i] * step;
-    }
     double elapsed = end_time - start_time;
 
     printf("Result: %lf; " , pi);
