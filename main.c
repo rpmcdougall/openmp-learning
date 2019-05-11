a1d84e4632086691ecb9e4c570800afd261731d1
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-10 19:31:55 -0500

    running parallel loop with reduction of sum to handle loop carried dependency.

--- main.c
+++ main.c
@@ -18,34 +18,63 @@
 /*
  * Threading (Fork Join), Pi SPMD Strategy, with Synchronization
  */
+//static long num_steps = 100000;
+//double step;
+//#define NUM_THREADS 2
+//void main() {
+//    double pi = 0.0;
+//    step = 1.0/(double) num_steps;
+//
+//    omp_set_num_threads(NUM_THREADS);
+//    double start_time = omp_get_wtime();
+//    #pragma omp parallel
+//    {
+//        int i, id, nthrds, nthreads;
+//        double x, sum;
+//        id = omp_get_thread_num();
+//        nthrds = omp_get_num_threads();
+//        if (id == 0) nthreads = nthrds;
+//        for (i=0, sum = 0.0; i<num_steps; i=i+nthreads) {
+//            x = (i + 0.5) * step;
+//            sum += 4.0/(1.0 + x*x);
+//        }
+//        // Only one thread summing at a time into pi
+//        #pragma omp critical
+//            pi += sum * step;
+//    };
+//
+//    double end_time = omp_get_wtime();
+//    double elapsed = end_time - start_time;
+//
+//    printf("Result: %lf; " , pi);
+//    printf("Elapsed Parallel: %lf", elapsed);
+//}
+
+/*
+ * Threading (Fork Join), Pi Parallel Loops
+ */
 static long num_steps = 100000;
 double step;
-#define NUM_THREADS 2
-void main() {
-    double pi = 0.0;
+int main()
+{
+    int i; double pi, sum = 0.0;
     step = 1.0/(double) num_steps;
 
-    omp_set_num_threads(NUM_THREADS);
     double start_time = omp_get_wtime();
     #pragma omp parallel
     {
-        int i, id, nthrds, nthreads;
-        double x, sum;
-        id = omp_get_thread_num();
-        nthrds = omp_get_num_threads();
-        if (id == 0) nthreads = nthrds;
-        for (i=0, sum = 0.0; i<num_steps; i=i+nthreads) {
-            x = (i + 0.5) * step;
-            sum += 4.0/(1.0 + x*x);
-        }
-        // Only one thread summing at a time into pi
-        #pragma omp critical
-            pi += sum * step;
-    };
-
-    double end_time = omp_get_wtime();
+        double x;
+        #pragma omp for reduction(+: sum)
+            for (i = 0; i < num_steps; i++) {
+                x = (i + 0.5) * step;
+                sum = sum + 4.0 / (1.0 + x * x);
+            }
+    }
+    pi = step * sum;
+        double end_time = omp_get_wtime();
     double elapsed = end_time - start_time;
 
     printf("Result: %lf; " , pi);
     printf("Elapsed Parallel: %lf", elapsed);
+
 }
